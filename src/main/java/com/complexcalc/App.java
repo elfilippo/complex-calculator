2c94914d10313f1be78415d50c530f12c5c390f1
Filip Main <theelfilippo@gmail.com>
2026-05-24 01:20:35 +0200

    fixed keyboard display bug, created custom keyboard

--- src/main/java/com/complexcalc/App.java
+++ src/main/java/com/complexcalc/App.java
@@ -5,8 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class App extends Application {
