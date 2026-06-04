f5f287b6dcdd7e31b7a2960fafaf606c86365920
Filip Main <theelfilippo@gmail.com>
2026-06-04 03:39:25 +0200

    added text view and touched up ui

--- src/main/java/com/complexcalc/ui/Controller.java
+++ src/main/java/com/complexcalc/ui/Controller.java
@@ -1,10 +1,12 @@
 package com.complexcalc.ui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.SplitPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.web.WebView;
@@ -18,9 +20,32 @@ public class Controller {
     private WebView latexWebPreview;
 
     @FXML
+    private SplitPane calcSplitPane;
+
+    @FXML
     public void initialize() {
         String url = getClass().getResource("/com/complexcalc/index.html").toExternalForm();
         latexWebPreview.getEngine().load(url);
+
+        latexWebPreview.setOnScroll(event -> {
+            if (event.isControlDown()) {
+                double zoom = latexWebPreview.getZoom();
+
+                if (event.getDeltaY() > 0) {
+                    zoom *= 1.1;
+                } else {
+                    zoom /= 1.1;
+                }
+
+                zoom = Math.max(0.5, Math.min(3.0, zoom));
+                latexWebPreview.setZoom(zoom);
+
+                event.consume();
+            }
+        });
+
+        calcSplitPane.setDividerPosition(0, 0.30);
+        calcSplitPane.setDividerPosition(1, 0.35);
 
         for (Node node : keyboardGrid.getChildren()) {
             if (node instanceof Button button) {
