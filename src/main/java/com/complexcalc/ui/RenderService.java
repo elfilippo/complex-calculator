153fafe35e633c46565a3f80760b2528de5f4e7b
Filip Laptop <theelfilippo@gmail.com>
2026-06-15 14:55:01 +0200

    fixed bugs where js string could be escaped

--- src/main/java/com/complexcalc/ui/RenderService.java
+++ src/main/java/com/complexcalc/ui/RenderService.java
@@ -12,12 +12,23 @@ public class RenderService {
     }
 
     public void render(String expression) {
-        escapedExpr = expression.replace("\\", "\\\\").replace("\"", "\\\"");
+        escapedExpr = expression
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+            .replace("'", "\\'")
+            .replace("\r", "\\r")
+            .replace("\n", "\\n")
+            .replace("\t", "\\t")
+            .replace("\b", "\\b")
+            .replace("\f", "\\f")
+            .replace("\u2028", "\\u2028")
+            .replace("\u2029", "\\u2029");
         String js = String.format(
             "document.getElementById('output').innerHTML = '\\\\[%s\\\\]';" +
                 "MathJax.typesetPromise([document.getElementById('output')]).then(() => window.javabridge.onRenderComplete());",
             escapedExpr
         );
+        System.out.println("JS = " + js);
         webEngine.executeScript(js);
     }
 
