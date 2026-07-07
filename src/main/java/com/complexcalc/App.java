dc1079fe999c2043327bc2c26493ee196de8c0c5
Filip Main <theelfilippo@gmail.com>
2026-07-07 02:18:11 +0200

    version 0.7.0 release

--- src/main/java/com/complexcalc/App.java
+++ src/main/java/com/complexcalc/App.java
@@ -1,7 +1,6 @@
 package com.complexcalc;
 
 import com.complexcalc.ui.Controller;
-import com.complexcalc.ui.RenderService;
 import com.complexcalc.ui.UIManager;
 import com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
 import java.io.IOException;
@@ -24,7 +23,7 @@ public class App extends Application {
     @Override
     @SuppressWarnings("exports")
     public void start(Stage stage) throws IOException {
-        stage.setTitle("complex-calculator v0.6.0 ©Filip M. 2026");
+        stage.setTitle("complex-calculator v0.7.0 ©Filip M. 2026");
         stage.setHeight(600);
         stage.setWidth(800);
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/complexcalc/textures/calculatorIcon.png")));
---
+++ src/main/java/com/complexcalc/Launcher.java
@@ -0,0 +1,11 @@
+package com.complexcalc;
+
+/**
+ * used to run jar
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
